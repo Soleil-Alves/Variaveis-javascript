5c89fe26a4adda6f6d5fbcc6f9a9f9687ee19fbf
Soleil Emilio Alves <92950798+Soleil-Alves@users.noreply.github.com>
2023-06-03 21:11:47 -0300

    Add files via upload

---
+++ calculation.js
@@ -0,0 +1,34 @@
+var x = 5;
+var y = 10;
+
+//soma//
+console.log ("A soma é " + (x + y))
+
+//divisão//
+console.log(y / x);
+
+//multiplicação//
+console.log(x * y);
+
+//subtração//
+console.log(x - y);
+
+//subtração//
+console.log(y - x);
+
+//resto//
+console.log(x % y);
+
+//Loops//
+
+while (x <= 40) {
+    console.log(x)
+
+    x *= 5;
+}
+
+while (y <= 50) {
+    console.log(y)
+
+    y *= 2; 
+}
